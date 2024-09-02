8163c9d293af35ef54c78e0b760119b2ae8404dc
FredsonNascimento <110387521+Fredson-D-santos@users.noreply.github.com>
2024-09-02 18:09:28 -0300

    Add files via upload

---
+++ pratic/main.js
@@ -0,0 +1,8 @@
+alert("Olá"); // Exibe uma mensagem de alerta
+
+var nome = prompt("Qual é o seu nome?"); // Pede ao usuário para inserir o nome e armazena na variável nome
+
+if (nome !== null) { // Verifica se o usuário não clicou em "Cancelar"
+    alert(`Olá, ${nome}!`); // Exibe uma mensagem de alerta com o nome digitado pelo usuário
+}
+
